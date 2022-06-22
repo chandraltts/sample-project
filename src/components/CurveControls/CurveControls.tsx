dd85a8a625e1ca2054297cbd13de2b595f60e5b6
Konatham Chandra Sekhar <konatham.chandrasekhar@ltts.com>
2022-06-22 11:37:45 +0530

    my first commit

--- src/components/CurveControls/CurveControls.tsx
+++ src/components/CurveControls/CurveControls.tsx
@@ -1,81 +1,105 @@
-import React, { useState, useEffect } from 'react';
-import { GroupData } from '../../containers/PlotBuilder/Model/data.model';
-import { Radio, Row, Col, Button, Tooltip } from 'antd';
-import { RadioChangeEvent } from 'antd/lib/radio';
-import CurveSelection from './CurveSelection/CurveSelection'
-import './CurveControls.css';
+import React, { useState, useEffect } from "react";
+import { GroupData } from "../../containers/PlotBuilder/Model/data.model";
+import { Radio, Row, Col, Button, Tooltip } from "antd";
+import { RadioChangeEvent } from "antd/lib/radio";
+import CurveSelection from "./CurveSelection/CurveSelection";
+import "./CurveControls.css";
 
 interface CurveControlsProps {
-   currentGroup: number;
-   groupData: GroupData[];
-   onCheck: any;
-   measurement: string;
-   convertToTrue: () => void;
-};
+  currentGroup: number;
+  groupData: GroupData[];
+  onCheck: any;
+  measurement: string;
+  convertToTrue: () => void;
+}
 
 const CurveControls: React.FC<CurveControlsProps> = (props) => {
-    const [group,setGroup] = useState(0);
+  const [group, setGroup] = useState(0);
 
-    useEffect( () => {
-      setGroup(props.currentGroup)   
-    });
+  useEffect(() => {
+    setGroup(props.currentGroup);
+  });
 
-    const onChangeGroup = (e: RadioChangeEvent) => {
-        console.log('radio checked', e.target.value);
-        const group_index = e.target.value;
-        setGroup(group_index);
-        const keys = [...props.groupData[group_index].keys];
-        props.onCheck(keys,group_index)
-    };
+  const onChangeGroup = (e: RadioChangeEvent) => {
+    console.log("radio checked", e.target.value);
+    const group_index = e.target.value;
+    setGroup(group_index);
+    const keys = [...props.groupData[group_index].keys];
+    props.onCheck(keys, group_index);
+  };
 
-    const onCheckCurve = (e) => {
-        props.onCheck(e,group);
-    }
+  const onCheckCurve = (e) => {
+    props.onCheck(e, group);
+  };
 
-    const submit = (e: any) => {
-        props.convertToTrue();
-    }
+  const submit = (e: any) => {
+    props.convertToTrue();
+  };
 
-    return(
-        <>
-        <div style={{height: '400px',borderStyle: 'solid', borderWidth: '2px',  borderColor: '#d9d9d9', margin: 'auto', padding: '10px', width: '100%'}}>
-          <div className="curve-title">
-            Curves
-          </div>
-          <Row style={{paddingTop: '10px', paddingBottom: '10px'}}>
-            <Col span={10}>
-                <div className="curve-measurement">
-                <strong>Data Type: </strong>{props.measurement}
-                </div>
-            </Col>
-            <Col  span={14}>
-                {(props.measurement==='engineering'?true:false)&&
-                <Tooltip title="Convert all curves from engineering to true values. WARNING: All results will be reset">
-                <Button style={{fontSize: '11px',float: 'right'}} size="small"  type="primary"  disabled={(props.measurement==='engineering'?false:true)} onClick={submit}>Convert to True</Button>
-                </Tooltip>}
-            </Col>
-          </Row>
-          <Row>
-            <Col>
-                <Radio.Group onChange={onChangeGroup} value={group}>{
-                    props.groupData.map( (g,index) => {
-                        return(
-                        <Radio key={index}  value={index} className="curve-group-title">{g.title}</Radio>
-                        );
-                    })
-                }</Radio.Group>
-            </Col>
-          </Row>
-          <Row style={{ width: '100%',  paddingTop: '10px'}}>
-            <Col>
-                <CurveSelection 
-                    treeData = {props.groupData[group].treeData}
-                    checkedKeys = {props.groupData[group].keys}
-                    onCheck = {onCheckCurve}
-                />
-            </Col>
-          </Row>
-          {/* <br/>
+  return (
+    <>
+      <div
+        style={{
+          height: "400px",
+          borderStyle: "solid",
+          borderWidth: "2px",
+          borderColor: "#d9d9d9",
+          margin: "auto",
+          padding: "10px",
+          width: "100%",
+        }}
+      >
+        <div className="curve-title">Curves</div>
+        <Row style={{ paddingTop: "10px", paddingBottom: "10px" }}>
+          <Col span={10}>
+            <div className="curve-measurement">
+              <strong>Data Type: </strong>
+              {props.measurement}
+            </div>
+          </Col>
+          <Col span={14}>
+            {(props.measurement === "engineering" ? true : false) && (
+              <Tooltip title="Convert all curves from engineering to true values. WARNING: All results will be reset">
+                <Button
+                  style={{ fontSize: "11px", float: "right" }}
+                  size="small"
+                  type="primary"
+                  disabled={props.measurement === "engineering" ? false : true}
+                  onClick={submit}
+                >
+                  Convert to True
+                </Button>
+              </Tooltip>
+            )}
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+            <Radio.Group onChange={onChangeGroup} value={group}>
+              {props.groupData.map((g, index) => {
+                return (
+                  <Radio
+                    key={index}
+                    value={index}
+                    className="curve-group-title"
+                  >
+                    {g.title}
+                  </Radio>
+                );
+              })}
+            </Radio.Group>
+          </Col>
+        </Row>
+        <Row style={{ width: "100%", paddingTop: "10px" }}>
+          <Col>
+            <CurveSelection
+              treeData={props.groupData[group].treeData}
+              checkedKeys={props.groupData[group].keys}
+              onCheck={onCheckCurve}
+            />
+          </Col>
+        </Row>
+        {/* <br/>
           <Space align='start'>
             <Radio.Group onChange={onChangeGroup} value={group}>{
                 props.groupData.map( (g,index) => {
@@ -90,8 +114,8 @@ const CurveControls: React.FC<CurveControlsProps> = (props) => {
                     onCheck = {onCheckCurve}
                 />
           </Space> */}
-        </div>
-        </>
-    );
-}
+      </div>
+    </>
+  );
+};
 export default CurveControls;
